3249254419f4cdde69ce50821ff7a3a16595d7ad
kafene <kafene@users.noreply.github.com>
2013-11-01 12:13:55 -0400

    minor improvements

--- HttpAuthDigest.php
+++ HttpAuthDigest.php
@@ -2,7 +2,7 @@
 
 namespace kafene;
 
-/**
+/*
 # kafene\HttpAuthDigest
 
 A PHP class for user logins via HTTP Digest Authentication.
@@ -30,63 +30,95 @@ This is free and unencumbered software released into the public domain.
 For more information, please refer to <http://unlicense.org/>
 */
 
-class HttpAuthDigest {
-    # Required elements in the parsed digest array.
-    protected $required = [
-        'algorithm', 'realm', 'uri', 'username', 'nonce',
-        'cnonce', 'opaque', 'qop', 'nc', 'response'
+class HttpAuthDigest
+{
+    # @var String
+    protected $ip = '';
+
+    # @var PDO
+    protected $db = null;
+
+    # @var Array
+    protected $users = [];
+
+    # @var String
+    protected $realm = '';
+
+    # @var Integer
+    protected $timeout = 0;
+
+    # @var Integer
+    protected $maxattempts = 0;
+
+    # @var Boolean
+    protected $halt = true;
+
+    # Database Schema
+    protected $dbSchema = [
+        'ip TEXT NOT NULL UNIQUE',
+        'num_attempts INTEGER NOT NULL'
+        'first_attempt_time INTEGER NOT NULL',
     ];
-    protected $ip = ''; # @var String
-    protected $db = null; # @var PDO
-    protected $users = []; # @var Array
-    protected $realm = ''; # @var String
-    protected $timeout = 0; # @var Integer
-    protected $maxattempts = 0; # @var Integer
-    protected $lockfile = ''; # @var String
-    protected $halt = true; # @var Boolean
+
+    # SQL strings
+    protected $sql = [
+        'delete' => 'DELETE FROM attempts WHERE first_attempt_time < %d',
+        'select' => 'SELECT num_attempts FROM attempts WHERE ip = ?',
+        'insert' => 'INSERT INTO attempts (ip, num_attempts, first_attempt_time) VALUES (?, ?, ?)'
+        'update' => 'UPDATE attempts SET num_attempts = num_attempts + 1 WHERE ip = ?';
+    ];
 
     /**
      * Initialize the object, set the realm.
      *
      * @param array $options All are optional. Consists of:
      *     - realm: Realm to use
-     *     - lockfile: filename of sqlite db file.
      *     - timeout: Timeout interval before subsequent auth attempts are stopped.
      *     - maxattempts: Max attempts allowed during the timeout interval.
      *     - halt: Stop further auth attempts by exiting the script.
      *     - users: Array of users => passwords or A1 values
      *     - password_a1: If the values in the users array are A1 values.
+     * @param PDO $db PDO Connection for database (for logging login attempts).
      * @return kafene\HttpAuth $this or the result of $this->authenticate().
      */
-    public function __construct(array $options = []) {
-        $this->realm = isset($options['realm'])
-            ? $options['realm']
-            : (getenv('SERVER_NAME') ?: 'PROTECTED');
-  
-        $this->lockfile = isset($options['lockfile'])
-            ? $options['lockfile']
-            : 'http_auth_lock.db';
+    public function __construct(array $options = array(), \PDO $db)
+    {
+        $this->realm = empty($options['realm'])
+            ? (getenv('SERVER_NAME') ?: 'PROTECTED')
+            : (string) $options['realm'];
 
-        $this->timeout = isset($options['timeout'])
-            ? (int) $options['timeout']
-            : 60; # Default 1 minute
-        
-        $this->maxattempts = isset($options['maxattempts'])
-            ? (int) $options['maxattempts']
-            : 3; # Default to 3 attempts per $timeout.
-        
-        $this->halt = isset($options['halt'])
-            ? (bool) $options['halt']
-            : true;
+        $this->timeout = empty($options['timeout'])
+            ? 60 # Default 1 minute
+            : (int) $options['timeout'];
+
+        $this->maxattempts = empty($options['maxattempts'])
+            ? 3 # Default to 3 attempts per $timeout.
+            : (int) $options['maxattempts'];
+
+        $this->halt = empty($options['halt'])
+            ? true
+            : (bool) $options['halt'];
+
+        $this->dbTable = empty($options['db_table'])
+            ? 'attempts'
+            : (string) $options['db_table'];
+
+        $this->db = $db;
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->db->exec(sprintf(
+            "PRAGMA secure_delete=1;".
+            "CREATE TABLE IF NOT EXISTS %s (%s)",
+            $this->dbTable,
+            join(', ', $this->dbSchema));
 
         # Add any users if specified.
-        if(isset($options['users'])) {
-            $password_a1 = isset($options['password_a1'])
-                ? (bool) $options['password_a1']
-                : false;
-            foreach($options['users'] as $user => $password) {
+        if (isset($options['users'])) {
+            $password_a1 = !empty($options['password_a1']);
+
+            foreach ($options['users'] as $user => $password) {
                 $this->addUser($user, $password, $password_a1);
             }
+
         }
 
         # Set up remote user IP.
@@ -105,7 +137,8 @@ class HttpAuthDigest {
      * @param boolean $password_a1 If the password is a pre-calculated A1.
      * @return kafene\HttpAuth $this
      */
-    public function addUser($user, $password = null, $password_a1 = false) {
+    public function addUser($user, $password = null, $password_a1 = false)
+    {
         $this->users[$user] = $password_a1
             ? $password
             : md5(sprintf('%s:%s:%s', $user, $this->realm, $password));
@@ -117,28 +150,34 @@ class HttpAuthDigest {
      *
      * @return boolean True if the user successfully authenticates.
      */
-    public function authenticate() {
-        $digest = $this->getDigest();
-        if(false !== $user = $this->getUser($digest)) {
-            $a1 = $this->users[$user];
-            $a2 = md5(sprintf(
-                '%s:%s',
-                getenv('REQUEST_METHOD') ?: 'GET',
-                $digest['uri']
-            ));
-            $valid = $digest['response'] === md5(sprintf(
-                '%s:%s:%s:%s:%s:%s',
-                $a1,
-                $digest['nonce'],
-                $digest['nc'],
-                $digest['cnonce'],
-                $digest['qop'],
-                $a2
-            ));
-            if($valid) {
-                return true;
+    public function authenticate()
+    {
+        if (array_key_exists('PHP_AUTH_DIGEST', $_SERVER)) {
+            $digest = $this->parseDigest($_SERVER['PHP_AUTH_DIGEST']);
+
+            if ($digest && (false !== $user = $this->getUser($digest))) {
+                $a1 = $this->users[$user];
+                $a2 = md5(sprintf(
+                    '%s:%s',
+                    getenv('REQUEST_METHOD') ?: 'GET',
+                    $digest['uri']
+                ));
+                $valid = md5(sprintf(
+                    '%s:%s:%s:%s:%s:%s',
+                    $a1,
+                    $digest['nonce'],
+                    $digest['nc'],
+                    $digest['cnonce'],
+                    $digest['qop'],
+                    $a2
+                ));
+
+                if ($digest['response'] === $valid) {
+                    return true;
+                }
             }
         }
+
         return $this->prompt();
     }
 
@@ -147,17 +186,24 @@ class HttpAuthDigest {
      * and increment the number of tries if so, otherwise log
      * this as the first validation attempt.
      */
-    protected function checkAttempt() {
-        if(0 === $attempts = $this->getAttempts()) {
+    protected function checkAttempt()
+    {
+        if (0 === $attempts = $this->getAttempts()) {
             $this->logAttempt(true);
             return true;
-        } elseif((int) $attempts > $this->maxattempts) {
-            if($this->halt) {
-                $errstr = 'You may not attempt to log in again for at least another %d seconds.';
+        }
+        elseif ((int) $attempts > $this->maxattempts) {
+
+            if ($this->halt) {
+                $errstr = 'You may not attempt to log in again ';
+                $errstr.= 'for at least another %d seconds.';
                 exit(sprintf($errstr, $this->timeout));
             }
-            throw new \Exception('Too many validation attempts, operation halted.', 500);
-        } else {
+
+            throw new \Exception('Too many validation attempts.', 500);
+
+        }
+        else {
             $this->logAttempt();
             return true;
         }
@@ -168,21 +214,36 @@ class HttpAuthDigest {
      * required keys are present.
      *
      * @param string $digest PHP Digest header
-     * @return array Parsed digest values
+     * @return array Parsed digest values or false on failure
      */
-    protected function getDigest() {
-        if(!empty($_SERVER['PHP_AUTH_DIGEST'])) {
-            $digest = $_SERVER['PHP_AUTH_DIGEST'];
-            if(preg_match_all('/(\w+)="?([^",]+)"?/', $digest, $m)) {
-                $recd = array_intersect_key($this->required, $m[1]);
-                $recd = array_filter(array_map('trim', $recd));
-                if($recd == $this->required && count($m[2]) === count($recd)) {
-                    return array_combine($m[1], $m[2]);
-                }
-                # @todo - maybe else {} -> log missing elements...
+    function parseDigest($digest)
+    {
+        $ret = [];
+        $keys = 'nonce|nc|cnonce|qop|username|uri|response|opaque|realm|algorithm';
+        $need = array_flip(explode('|', $keys));
+        $expr = '/(\w+)\s*=\s*(?:([\'"])([^\2]+?)\2|([^\s,&;]+))/x';
+
+        preg_match_all($expr, $digest, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $m) {
+            if (isset($m[1], $m[3])) {
+                $ret[$m[1]] = isset($m[4]) ? $m[4] : $m[3];
             }
         }
-        return $this->prompt();
+
+        $ret = array_intersect_key($ret, $need);
+        $valid = 0 === sizeof(array_diff_key($need, $ret));
+
+        if ($valid) {
+            $ret['username'] = str_replace(
+                ["\\\"", "\\\\"],
+                ["\"", "\\"],
+                $ret['username']
+            );
+            return $ret;
+        }
+
+        return false;
     }
 
     /**
@@ -192,11 +253,18 @@ class HttpAuthDigest {
      * @param array $digest parsed HTTP Digest header.
      * @return string The username, if found.
      */
-    protected function getUser($digest) {
-        if(empty($digest['username'])) { return $this->prompt(); }
+    protected function getUser($digest)
+    {
+        if (empty($digest['username'])) {
+            return $this->prompt();
+        }
+
         $user = $digest['username'];
-        if(empty($this->users[$user])) { return $this->prompt(); }
-        # var_dump($user, $digest, $this->users, $_SERVER['PHP_AUTH_DIGEST']);die;
+
+        if (empty($this->users[$user])) {
+            return $this->prompt();
+        }
+
         return $user;
     }
 
@@ -206,44 +274,27 @@ class HttpAuthDigest {
      *
      * @return boolean False
      */
-    protected function prompt() {
-        $this->openDatabase();
+    protected function prompt()
+    {
+        $old = time() - (int) $this->timeout);
+        $this->db->exec(sprintf($this->sql['delete'], $old));
         $this->checkAttempt();
+
         $message = 'HTTP/1.1 401 Unauthorized';
         header($message, true, 401);
+
         header(sprintf(
             'WWW-Authenticate: Digest realm="%s", qop="auth", nonce="%s", opaque="%s"',
             $this->realm,
             trim(base64_encode(uniqid('', true)), '='),
             md5($this->realm)
         ));
+
         if(false !== $this->halt) {
             exit($message);
         }
+
         return false;
-    }
-    
-    /**
-     * Open the database connection used for logging attempts.
-     *
-     * @return null
-     */
-    protected function openDatabase() {
-        $create = !file_exists($this->lockfile);
-        $this->db = new \PDO('sqlite:'.$this->lockfile);
-        $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        if($create) {
-            $this->db->query('CREATE TABLE IF NOT EXISTS attempts (
-                ip TEXT NOT NULL UNIQUE,
-                num_attempts INTEGER NOT NULL,
-                first_attempt_time INTEGER NOT NULL
-            )');
-        }
-        $this->db->exec('PRAGMA secure_delete=1');
-        # Clear validation attempts that are older than the validation interval.
-        $sql = 'DELETE FROM attempts WHERE first_attempt_time < %d';
-        $old = time() - (int) $this->timeout;
-        $this->db->exec(sprintf($sql, $old));
     }
 
     /**
@@ -251,12 +302,13 @@ class HttpAuthDigest {
      *
      * @return integer Number of attempts thus far.
      */
-    protected function getAttempts() {
-        $stmt = $this->db->prepare('SELECT num_attempts FROM attempts WHERE ip = ?');
-        $stmt->execute([$this->ip]);
-        $num_attempts = (int) $stmt->fetchColumn();
-        $stmt->closeCursor();
-        return $num_attempts;
+    protected function getAttempts()
+    {
+        $st = $this->db->prepare($this->sql['select']);
+        $st->execute([$this->ip]);
+        $attemptCount = (int) $st->fetchColumn();
+        $st->closeCursor();
+        return $attemptCount;
     }
 
     /**
@@ -265,16 +317,19 @@ class HttpAuthDigest {
      * @param boolean $first If it is the first validation attempt from this IP.
      * @return integer Last database insert ID.
      */
-    protected function logAttempt($first = false) {
-        $sql = $first
-            ? 'INSERT INTO attempts (ip, num_attempts, first_attempt_time) VALUES (?, ?, ?)'
-            : 'UPDATE attempts SET num_attempts = num_attempts + 1 WHERE ip = ?';
-        $params = $first
-            ? [$this->ip, 1, time()]
-            : [$this->ip];
-        #var_dump($sql, $params, $this->db->query('SELECT * FROM attempts')->fetchAll());die;
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($params);
+    protected function logAttempt($first = false)
+    {
+        if ($first) {
+            $sql = $this->sql['insert'];
+            $params = array($this->ip, 1, time());
+        } else {
+            $sql = $this->sql['update'];
+            $params = array($this->ip);
+        }
+
+        $st = $this->db->prepare($sql);
+        $st->execute($params);
+
         return $this->db->lastInsertId();
     }
 }
